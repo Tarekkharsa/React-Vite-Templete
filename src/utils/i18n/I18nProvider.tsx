fb6fd7c59fd66cd608aa32d2a05fc26a0a5b6dd0
Tarekkharsa <47394961+Tarekkharsa@users.noreply.github.com>
2022-06-30 16:53:57 +0300

    fix: install types types

--- src/utils/i18n/I18nProvider.tsx
+++ src/utils/i18n/I18nProvider.tsx
@@ -2,15 +2,15 @@ import { IntlProvider } from 'react-intl';
 
 import useLang from '@/hooks/useLang';
 
-import arMessages from './messages/ar';
-import enMessages from './messages/en';
+import arMessages from './messages/ar.json';
+import enMessages from './messages/en.json';
 
 const allMessages = {
   en: enMessages,
   ar: arMessages,
-};
+} as any;
 
-export function I18nProvider({ children }) {
+export function I18nProvider({ children }: any) {
   const { lang } = useLang();
   const messages = allMessages[lang];
 
